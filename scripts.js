8b07f9345c1e86f03e44efdd853d01309809220f
Jhoslandy <105742772+Jhoslandy@users.noreply.github.com>
2025-05-14 03:02:24 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Podrías agregar funciones interactivas aquí, por ejemplo un saludo navideño:
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("¡Feliz Navidad desde Urgencias!");
+});
